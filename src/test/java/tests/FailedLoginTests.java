ff759b27346a26b2ea74eb905110b008e8dcdf22
bartoszmonka <bartoszmonka@gmail.com>
2020-03-19 00:09:18 +0100

    add new failed login test cases

--- src/test/java/tests/FailedLoginTests.java
+++ src/test/java/tests/FailedLoginTests.java
@@ -24,7 +24,63 @@ public class FailedLoginTests extends TestBase {
         loginPage.typeIntoWrongUserEmailNameField();
         loginPage.typeIntoWrongUserPasswordField();
         loginPage.clickOnLoginButton();
-        loginPage.getWarningMessage();
+        loginPage.getAuthenticationMessage();
+    }
+
+    @Severity(SeverityLevel.NORMAL)
+    @Test(retryAnalyzer = RetryAnalyzer.class)
+    @Description("The steps of this test is using Invalid user email and not proper password" +
+            " check if warning message Invalid email address is displayed")
+    public void asUserTryToLogInWithInvalidEmailAndPassword() {
+        DriverUtils.navigateToPage(LOGIN_URL);
+
+        LoginPage loginPage = new LoginPage();
+        loginPage.typeIntoInvalidUserEmailNameField();
+        loginPage.typeIntoWrongUserPasswordField();
+        loginPage.clickOnLoginButton();
+        loginPage.getInvalidEmailMessage();
+    }
+
+    @Severity(SeverityLevel.CRITICAL)
+    @Test(retryAnalyzer = RetryAnalyzer.class)
+    @Description("The steps of this test is using proper user email and not proper password" +
+            " check if warning message Authentication failed is displayed")
+    public void asUserTryToLogInWithCorrectLoginAndWrongPassword() {
+        DriverUtils.navigateToPage(LOGIN_URL);
+
+        LoginPage loginPage = new LoginPage();
+        loginPage.typeIntoUserEmailField();
+        loginPage.typeIntoWrongUserPasswordField();
+        loginPage.clickOnLoginButton();
+        loginPage.getAuthenticationMessage();
+    }
+
+    @Severity(SeverityLevel.NORMAL)
+    @Test(retryAnalyzer = RetryAnalyzer.class)
+    @Description("The steps of this test is using proper user email and empty password field" +
+            " check if warning message Password is required is displayed")
+    public void asUserTryToLogInWithCorrectLoginAndEmptyPasswordField() {
+        DriverUtils.navigateToPage(LOGIN_URL);
+
+        LoginPage loginPage = new LoginPage();
+        loginPage.typeIntoUserEmailField();
+        loginPage.typeIntoEmptyFieldUserPassword();
+        loginPage.clickOnLoginButton();
+        loginPage.getPasswordIsRequiredMessage();
+    }
+
+    @Severity(SeverityLevel.NORMAL)
+    @Test(retryAnalyzer = RetryAnalyzer.class)
+    @Description("The steps of this test is using empty field email and password" +
+            " check if warning message An email address required is displayed")
+    public void asUserTryToLogInWithEmptyEmailFieldAndPassword() {
+        DriverUtils.navigateToPage(LOGIN_URL);
+
+        LoginPage loginPage = new LoginPage();
+        loginPage.typeIntoUserEmailEmptyField();
+        loginPage.typeIntoPasswordField();
+        loginPage.clickOnLoginButton();
+        loginPage.getEmailAddressIsRequiredMessage();
     }
 
 }
