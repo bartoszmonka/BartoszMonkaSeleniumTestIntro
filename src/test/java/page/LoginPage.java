fbfdc78fb3436bc682db93eff6178ac7933ab07b
bartoszmonka <44695739+bartoszmonka@users.noreply.github.com>
2020-03-18 10:57:03 +0100

    Update LoginPage.java
    add Sign out method

--- src/test/java/page/LoginPage.java
+++ src/test/java/page/LoginPage.java
@@ -7,14 +7,23 @@ import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.AssertJUnit;
 import waits.WaitForElement;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
+import static org.testng.AssertJUnit.assertTrue;
 
 public class LoginPage {
     private Logger logger = LogManager.getLogger(LoginPage.class);
+    String userEmail ="testMail88@gmail.com";
+    String userPassword ="JanGate";
+    String userWrongEmail ="wrongpwd42@gmail.com";
+    String userWrongPassword ="WrongWrong";
 
     @FindBy(id = "email")
     private WebElement emailField;
-
+    
     @FindBy(id = "passwd")
     private WebElement passwordField;
 
@@ -27,24 +36,40 @@ public class LoginPage {
     @FindBy(css = "#center_column > h1")
     private WebElement myAccountLabel;
 
+    @FindBy(css = "#header > div.nav > div > div > nav > div:nth-child(2) > a")
+    private WebElement signOut;
+
+
     public LoginPage() {
         PageFactory.initElements(DriverManager.getWebDriver(), this);
     }
 
     @Step("Type into User Name Field {username}")
-    public LoginPage typeIntoUserNameField(String username) {
+    public void typeIntoUserNameField() {
         WaitForElement.waitUntilElementIsVisible(emailField);
-        emailField.sendKeys(username);
-        logger.info("Typed into User Name Field {}", username);
-        return this;
+        emailField.sendKeys(userEmail);
+        logger.info("Typed into User Name Field {}", userEmail);
     }
 
     @Step("Type into Password Field {password}")
-    public LoginPage typeIntoPasswordField(String password) {
+    public void typeIntoPasswordField() {
         passwordField.clear();
-        passwordField.sendKeys(password);
-        logger.info("Typed into Password Field {}", password);
-        return this;
+        passwordField.sendKeys(userPassword);
+        logger.info("Typed into Password Field {}", userPassword);
+    }
+
+    @Step("Type into User Name Field {username}")
+    public void typeIntoWrongUserEmailNameField() {
+        WaitForElement.waitUntilElementIsVisible(emailField);
+        emailField.sendKeys(userWrongEmail);
+        logger.info("Typed into User Name Field {}", userWrongEmail);
+    }
+
+    @Step("Type into Password Field {password}")
+    public void typeIntoWrongUserPasswordField() {
+        passwordField.clear();
+        passwordField.sendKeys(userWrongPassword);
+        logger.info("Typed into Password Field {}", userWrongPassword);
     }
 
     @Step("Click on Login Button")
@@ -54,19 +79,30 @@ public class LoginPage {
     }
 
     @Step("Getting warning message from Login Page")
-    public String getWarningMessage() {
+    public void getWarningMessage() {
         WaitForElement.waitUntilElementIsVisible(messageLabel);
         String warningText = messageLabel.getText();
         logger.info("Returned warning message was: {}", warningText);
-        return warningText;
+        AssertJUnit.assertEquals(warningText, "Authentication failed.");
     }
 
     @Step("Getting MY ACCOUNT information from Login Page")
-    public String getMyAccount() {
+    public void getMyAccount() {
         WaitForElement.waitUntilElementIsVisible(myAccountLabel);
         String myAccountText = myAccountLabel.getText();
         logger.info("Returned MY ACCOUNT: {}", myAccountText);
-        return myAccountText;
+        assertEquals(myAccountText, "MY ACCOUNT");
+    }
+    @Step("Click Sign Out from Login Page")
+    public void signOutClick(){
+        signOut.click();
+        logger.info("Click Sign Out");
+    }
+    @Step("Check Sign In Button is Displayed in page")
+    public void checkCorrectSignOut(){
+        WaitForElement.waitUntilElementIsVisible(signInButton);
+        assertTrue(signInButton.isDisplayed());
+        logger.info("sign In Button is Displayed");
     }
 
 }
