d6fcd06a720c6cf2c01f671fd59e54e33de09be4
bartoszmonka <bartoszmonka@gmail.com>
2020-10-26 17:44:40 +0100

    update

--- src/test/java/driver/BrowserFactory.java
+++ src/test/java/driver/BrowserFactory.java
@@ -11,7 +11,6 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.ie.InternetExplorerOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-
 import java.net.MalformedURLException;
 import java.net.URL;
 
